69bb0cd2511ce761f08365e31297a574ee7f84be
USER <user@Nehas-MacBook-Air.local>
2020-07-02 15:53:08 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get("/posts/:postName",function(req,res){
 
 let port = process.env.PORT;
 if (port === null || port === "") {
-  port = 4000;
+  port = 3000;
 }
 
 app.listen(port, function() {
